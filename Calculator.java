e6e5e0dc48d0aa999845a61a7d946d4f660c0935
FelipeAP04 <agu23195@uvg.edu.gt>
2024-01-30 16:57:44 -0600

    implementacion de codigo

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,32 @@
 public class Calculator {
+
     public int evaluatePostfix(String expression, UVGStack<Integer> stack) {
         for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
 
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0'); 
+            } else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (ch) {
+                    case '+':
+                        stack.push(val2 + val1);
+                        break;
+                    case '-':
+                        stack.push(val2 - val1);
+                        break;
+                    case '*':
+                        stack.push(val2 * val1);
+                        break;
+                    case '/':
+                        stack.push(val2 / val1);
+                        break;
+                }
+            }
         }
+
+        return stack.pop();
     }
 }
