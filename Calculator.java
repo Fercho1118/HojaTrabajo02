f1a78436039defebc2d9d3ee5fca341cf1020fc2
FelipeAP04 <agu23195@uvg.edu.gt>
2024-01-30 16:39:49 -0600

    recorrer

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,7 @@
 public class Calculator {
-    
+    public int evaluatePostfix(String expression, UVGStack<Integer> stack) {
+        for (int i = 0; i < expression.length(); i++) {
+
+        }
+    }
 }
