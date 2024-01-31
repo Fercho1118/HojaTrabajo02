96a96e4c1c0a39ad24e02c239c5e9632c73a0b17
Fercho1118 <rue23748@uvg.edu.gt>
2024-01-30 20:10:02 -0600

    Corrección de errores
    Se arregló un NullPointerException, este se debió a la forma en que manejamos los operadores en el método evaluatePostfix de la clase Calculator.

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,24 @@
 public class Calculator {
 
     public int evaluatePostfix(String expression, UVGStack<Integer> stack) {
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
+        String[] tokens = expression.split(" ");
 
-            if (Character.isDigit(ch)) {
-                stack.push(ch - '0'); 
+        for (String token : tokens) {
+
+            if (token.matches("\\d+")) {
+                stack.push(Integer.parseInt(token)); 
             } else {
+                if (stack.isEmpty()) {
+                    throw new RuntimeException("Faltan operandos para el operador " + token);
+                }
                 int val1 = stack.pop();
+
+                if (stack.isEmpty()){
+                    throw new RuntimeException("Faltan operando para el operador " + token);
+                }
                 int val2 = stack.pop();
 
-                switch (ch) {
+                switch (token.charAt(0)) {
                     case '+':
                         stack.push(val2 + val1);
                         break;
