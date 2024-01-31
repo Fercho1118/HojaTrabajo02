990e9c4362891f3ee4981af49d6acd11fedf737e
FelipeAP04 <agu23195@uvg.edu.gt>
2024-01-30 22:09:34 -0600

    Unit Test

---
+++ DriverTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Driver;
+
+import org.junit.jupiter.api.Test;
+
+
+class DriverTest {
+
+	@Test
+	void testCalculator() {
+		Driver driver = new Driver();
+		assertEquals(radio.getMessage(), integer);
+	}
+
+}
